7c045595f40ddbe8a4293a034e66b00c4f5ab5b9
Winner Edwin <winneredwin2667@gmail.com>
2025-08-18 23:52:23 +0100

    Update socials

--- src/constants/globals.ts
+++ src/constants/globals.ts
@@ -1,5 +1,5 @@
 export const GITHUB_URL = "https://github.com/mikelneonedwin";
-export const FACEBOOK_URL = "https://www.facebook.com/winneredwin2667";
+export const FACEBOOK_URL = "https://fb.me/winneredwin2667";
 export const X_URL = "https://x.com/mikelneonedwin";
 export const EMAIL = "winneredwin2667@gmail.com";
 export const EMAIL_URL = `mailto:${EMAIL}`;
